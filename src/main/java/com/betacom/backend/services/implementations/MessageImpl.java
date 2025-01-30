6386a9dde8a8eb4718ae18d2e87b0d4ed575699b
b-v-Daniel01 <bostanvasiledaniel@gmail.com>
2025-01-30 21:30:20 +0100

    Messages model e service

---
+++ src/main/java/com/betacom/backend/services/implementations/MessageImpl.java
@@ -0,0 +1,35 @@
+package com.betacom.backend.services.implementations;
+
+import com.betacom.backend.model.MessageID;
+import com.betacom.backend.repositories.IMessageRepository;
+import com.betacom.backend.services.interfaces.MessageServices;
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MessageImpl implements MessageServices {
+    @Value("${langMsg}")
+    String langMsg;
+
+    @Autowired
+    IMessageRepository msgR;
+
+    @Override
+    public String getMessage(String code) {
+        Optional<Message> msg = msgR.findById( new MessageID(langMsg,code) );
+
+        String res = null;
+
+        if(msg.isEmpty())
+            res = code;
+        else
+            res = msg.get().getMessage();
+
+        return res;
+
+    }
+}
