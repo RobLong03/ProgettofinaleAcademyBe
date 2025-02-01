34684edc727824090508e11e9f8399d74d3b1856
b-v-Daniel01 <bostanvasiledaniel@gmail.com>
2025-02-01 20:11:39 +0100

    getters and setters di price in product

--- src/main/java/com/betacom/backend/model/Product.java
+++ src/main/java/com/betacom/backend/model/Product.java
@@ -76,6 +76,14 @@ public class Product {
 		this.stock = stock;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 	public Product() {
 	}
 
